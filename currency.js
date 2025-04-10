6178c0a63db6521167b082e466c9084da831f332
P.Bala kishore <129872649+Kishore0122@users.noreply.github.com>
2025-04-10 09:04:26 +0530

    Add files via upload

---
+++ currency.js
@@ -0,0 +1,233 @@
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+  }; 
+  
+
+  const baseurl = "https://v6.exchangerate-api.com/v6/9a9546d65a01e63636982140/latest";
+  const dropdowm = document.querySelectorAll(".dropdowm select");
+  const formcurr = document.querySelector(".form select");
+  const tocurr = document.querySelector(".to select");
+  const btn = document.querySelector("form button");
+  const msg = document.querySelector(".msg");
+  
+  for (let select of dropdowm) {
+    for (let currcode in countryList) {
+      let newOption = document.createElement("option");
+      newOption.innerText = currcode;
+      newOption.value = currcode;
+  
+      if (select.name === "form" && currcode === "USD") {
+        newOption.selected = true;
+      } else if (select.name === "to" && currcode === "INR") {
+        newOption.selected = true;
+      }
+  
+      select.appendChild(newOption);
+    }
+  
+    select.addEventListener("change", (evt) => {
+      updateFlag(evt.target);
+    });
+  }
+  
+  const updateFlag = (element) => {
+    let currcode = element.value;
+    let countrycode = countryList[currcode];
+    let newsrc = `https://flagsapi.com/${countrycode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    if (img) {
+      img.src = newsrc;
+    }
+  };
+  
+  const updateExchange = async () => {
+    let amount = document.querySelector(".amount input");
+    let amountval = amount.value;
+  
+    if (amountval === "" || amountval < 1) {
+      amountval = 1;
+      amount.value = "1";
+    }
+  
+    const URL = `${baseurl}/${formcurr.value}`;
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+  
+      const rate = data.conversion_rates[tocurr.value];
+      const converted = (amountval * rate).toFixed(2);
+  
+      msg.innerText = `${amountval} ${formcurr.value} = ${converted} ${tocurr.value}`;
+    } catch (err) {
+      console.error("Fetch error:", err);
+      msg.innerText = "Error fetching exchange rate.";
+    }
+  };
+  
+  btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchange();
+  });
+  window.addEventListener("load", () => {
+    updateFlag(formcurr);
+    updateFlag(tocurr);
+    updateExchange();
+  });
